16443c32393cf728d4cfdc89fded2f432ddfabae
wizardy <wizardhassan99@gmail.com>
2023-09-10 20:22:37 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>EH Coding Home Page</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis similique voluptatibus libero non cumque, odit itaque minima ipsum officia earum. Harum quia sequi ipsum, dicta sint necessitatibus quo aspernatur architecto!</p>
     </div>
   )
 }
