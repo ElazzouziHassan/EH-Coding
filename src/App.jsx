1df9311b1e85288894491051ed6b015bd7b5bf56
wizardy <wizardhassan99@gmail.com>
2023-09-10 21:15:42 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Home from "./pages/home/Home"
 
 function App() {
 
   return (
     <div className="App">
       <h1>EH Coding Home Page</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis similique voluptatibus libero non cumque, odit itaque minima ipsum officia earum. Harum quia sequi ipsum, dicta sint necessitatibus quo aspernatur architecto!</p>
+      <Home/>
     </div>
   )
 }
