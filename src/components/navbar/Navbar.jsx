9537bd65f9a5e88e06d7ecdf4e4687cfb311bfe7
wizardy <wizardhassan99@gmail.com>
2023-09-10 21:40:26 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import './navbar.scss'
+import { useContext } from 'react';
+import { DarkModeContext } from '../../context/darkModeContext';
+import { Link } from 'react-router-dom';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import OtherHousesOutlinedIcon from '@mui/icons-material/OtherHousesOutlined';
+import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
+import TerminalOutlinedIcon from '@mui/icons-material/TerminalOutlined';
+import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
+import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
+import PermContactCalendarOutlinedIcon from '@mui/icons-material/PermContactCalendarOutlined';
+
 
 function Navbar() {
+
+  const { toggle, darkMode } = useContext(DarkModeContext);
+
   return (
-    <div>Navbar</div>
+    <div className="nav-bar">
+      <div className="left">
+        <div className="logo">
+          <Link to='/'><h2>EH<span>.Coding</span></h2></Link>
+        </div>
+      </div>
+      <div className="menu">
+        <ul>
+          <Link to='/'>
+            <li>
+              <OtherHousesOutlinedIcon className="icon" />
+              <span>HOME</span>
+            </li>
+          </Link>
+          <Link to='/about'> 
+            <li>
+              <CodeOutlinedIcon className="icon" />
+              <span>ABOUT</span>
+            </li>
+          </Link>
+          <Link to='/services'>            
+            <li><EngineeringOutlinedIcon className="icon" /> <span>SERVICES</span></li>
+          </Link>
+          <Link to='/works'>            
+            <li><TerminalOutlinedIcon className="icon" /> <span>WORKS</span></li>
+          </Link>
+          <Link to='resume'>           
+            <li><BadgeOutlinedIcon className="icon" /> <span>RESUME</span></li>
+          </Link>
+        </ul>
+      </div>
+      <div className="right">
+        <button onClick={toggle}>
+          {
+            darkMode ? <LightModeOutlinedIcon className='icon'/> : <DarkModeOutlinedIcon className='icon'/>
+          }
+        </button>
+        <Link to="/contact">
+          <button>
+            <PermContactCalendarOutlinedIcon className='icon' />
+          </button>
+        </Link>
+      </div>     
+    </div>
   )
 }
 
