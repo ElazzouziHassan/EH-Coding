d3cc2dd9d06890e1bc60f0d032716a6e746ffc92
wizardy <wizardhassan99@gmail.com>
2023-09-11 13:25:49 +0100

    commit

--- src/components/home-components/sub-hero/Subhero.jsx
+++ src/components/home-components/sub-hero/Subhero.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './subhero.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Subhero() {
   return (
-    <div>Subhero</div>
+    <div className="sub-hero">
+      <div className="hr">
+        <hr />
+      </div>
+      <div className="social-media">
+      <a href='#'>
+            <GitHubIcon className='s-icon github' />
+            <span>GitHub</span>
+          </a>
+          <a href='#'>
+            <LinkedInIcon className='s-icon linkedIn' />
+            <span>LinkedIn</span>
+          </a>
+          <a href='#'>
+            <FacebookIcon className='s-icon facebook' />
+            <span>Facebook</span>
+          </a>
+          <a href='#'>
+            <TwitterIcon className='s-icon twitter' />
+            <span>Twitter</span>
+          </a>
+          <a href='#'>
+            <InstagramIcon className='s-icon instagram' />
+            <span>Instagram</span>
+          </a>
+      </div>
+    </div>
   )
 }
 
