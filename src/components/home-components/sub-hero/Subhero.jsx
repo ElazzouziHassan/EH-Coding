7d539845ad15a8bbe5e3df41977521c9f5152e56
wizardy <wizardhassan99@gmail.com>
2023-09-17 19:02:34 +0100

    commit

---
+++ src/components/home-components/sub-hero/Subhero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './subhero.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+function Subhero() {
+  return (
+    <div className="sub-hero">
+      
+    </div>
+  )
+}
+
+export default Subhero
