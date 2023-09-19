63460cb89eb3503cdd0de02d0a30493fde43693d
wizardy <wizardhassan99@gmail.com>
2023-09-19 21:03:12 +0100

    commit

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './services.scss'
 
 function Services() {
   return (
     <div className="services">
-      <h1>services</h1>
+      <h1>Services page</h1>
     </div>
   )
 }
