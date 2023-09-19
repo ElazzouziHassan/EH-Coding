4f571f97f6e858e1f30b48b559572a148c1028a0
wizardy <wizardhassan99@gmail.com>
2023-09-19 21:24:57 +0100

    commit

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -5,7 +5,42 @@ function Services() {
   return (
     <div className="services">
       <h2>Services</h2>
-      
+      <p>
+        As a Full Stack Developer and UI/UX Designer I ready to provide my clients with clean and high-quality services.
+      </p>
+      <span>
+      What am I offering?
+      </span>
+      <div className="services-container">
+        <div className="service ui-ux">
+          <img src="#" alt="UI/UI Design" />
+          <h2>UI/UI Design</h2>
+        </div>
+        <div className="service frontend">
+          <img src="#" alt="front-end development" />
+          <h2>front-end development</h2>
+        </div>
+        <div className="service backend">
+          <img src="#" alt="Back-end development" />
+          <h2>Back-end development</h2>
+        </div>
+        <div className="service web-app">
+          <img src="#" alt="Custom Web Applications" />
+          <h2>Custom Web Applications</h2>
+        </div>
+        <div className="service wordpress">
+          <img src="#" alt="WordPress and eCommerce websites" />
+          <h2>WordPress and eCommerce websites</h2>
+        </div>
+        <div className="service database">
+          <img src="#" alt="databases" />
+          <h2>databases</h2>
+        </div>
+        <div className="service api">
+          <img src="#" alt="REST API Developement" />
+          <h2>REST API Developement</h2>
+        </div>
+      </div>
     </div>
   )
 }
