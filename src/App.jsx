7248c237827336dd082407fdeff6c2bec071da0e
wizardy <wizardhassan99@gmail.com>
2023-09-20 20:13:37 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-// import About from "./pages/about/About";
-// import Contact from "./pages/contact/Contact";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
 import Home from "./pages/home/Home";
-// import Resume from "./pages/resume/Resume";
+import Resume from "./pages/resume/Resume";
 import Services from "./pages/services/Services";
-// import Works from "./pages/works/Works";
+import Works from "./pages/works/Works";
 import Navbar from "./components/navbar/Navbar"
 
 
@@ -27,11 +27,11 @@ function App() {
     { path: '/', element: <Layout />,
       children: [
         { path: '/', element: <Home /> },
-        // { path: '/about', element: <About /> },
+        { path: '/about', element: <About /> },
         { path: '/services', element: <Services/> },
-        // { path: '/works', element: <Works /> },
-        // { path: '/resume', element: <Resume /> },
-        // { path: '/contact', element: <Contact /> },
+        { path: '/works', element: <Works /> },
+        { path: '/resume', element: <Resume /> },
+        { path: '/contact', element: <Contact /> },
       ]
     },
   ]);
