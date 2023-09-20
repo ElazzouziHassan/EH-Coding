da75b6481e77e1046a7273c2cf23f54e6d6f2bc9
wizardy <wizardhassan99@gmail.com>
2023-09-20 20:50:33 +0100

    commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import './about.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function About() {
   return (
     <div className="about">
       <div className="wrapper">
         <h1>coming soon<span className="dot">.</span></h1>
-        <p>Put some text here.</p>
+        <p>Try to connect with me on social media</p>
         <div className="icons">
-          <a href=""><i className="fa fa-twitter"></i></a>
-          <a href=""><i className="fa fa-youtube-play"></i></a>
-          <a href=""><i className="fa fa-paper-plane"></i></a>
+        <a href='https://github.com/ElazzouziHassan' target='_blank'>
+                <GitHubIcon className='s-icon github' />
+            </a>
+              <a href='https://www.linkedin.com/in/elazzouzihassan/' target='_blank'>
+                <LinkedInIcon className='s-icon linkedIn' />
+              </a>
+              <a href='https://www.facebook.com/itsmewizardy' target='_blank'>
+                <FacebookIcon className='s-icon facebook' />
+              </a>
+              <a href='https://twitter.com/itsmewizardy' target='_blank'>
+                <TwitterIcon className='s-icon twitter' />
+              </a>
         </div>
       </div>
     </div>
