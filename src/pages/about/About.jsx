12e5f050c2863b84a3b6106b588a736b3c930e30
wizardy <wizardhassan99@gmail.com>
2023-09-20 20:55:59 +0100

    commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './about.scss'
 import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
