5035199aa284d7601bc143f9ae61d04beff038fb
wizardy <wizardhassan99@gmail.com>
2023-09-20 21:00:03 +0100

    commit

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,44 +1,29 @@
 import React from 'react'
 import './services.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Services() {
   return (
     <div className="services">
-      <h2>Services</h2>
-      <p>
-        As a Full Stack Developer and UI/UX Designer I ready to provide my clients with clean and high-quality services.
-      </p>
-      <span>
-      What am I offering?
-      </span>
-      <div className="services-container">
-        <div className="service ui-ux">
-          <img src="#" alt="UI/UI Design" />
-          <h2>UI/UI Design</h2>
-        </div>
-        <div className="service frontend">
-          <img src="#" alt="front-end development" />
-          <h2>front-end development</h2>
-        </div>
-        <div className="service backend">
-          <img src="#" alt="Back-end development" />
-          <h2>Back-end development</h2>
-        </div>
-        <div className="service web-app">
-          <img src="#" alt="Custom Web Applications" />
-          <h2>Custom Web Applications</h2>
-        </div>
-        <div className="service wordpress">
-          <img src="#" alt="WordPress and eCommerce websites" />
-          <h2>WordPress and eCommerce websites</h2>
-        </div>
-        <div className="service database">
-          <img src="#" alt="databases" />
-          <h2>databases</h2>
-        </div>
-        <div className="service api">
-          <img src="#" alt="REST API Developement" />
-          <h2>REST API Developement</h2>
+      <div className="wrapper">
+        <h1>coming soon<span className="dot">.</span></h1>
+        <p>Try to connect with me on social media</p>
+        <div className="icons">
+        <a href='https://github.com/ElazzouziHassan' target='_blank'>
+                <GitHubIcon className='s-icon github' />
+            </a>
+              <a href='https://www.linkedin.com/in/elazzouzihassan/' target='_blank'>
+                <LinkedInIcon className='s-icon linkedIn' />
+              </a>
+              <a href='https://www.facebook.com/itsmewizardy' target='_blank'>
+                <FacebookIcon className='s-icon facebook' />
+              </a>
+              <a href='https://twitter.com/itsmewizardy' target='_blank'>
+                <TwitterIcon className='s-icon twitter' />
+              </a>
         </div>
       </div>
     </div>
