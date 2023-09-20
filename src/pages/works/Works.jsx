4e320bc352e94c36320abf3b955508fe468999e8
wizardy <wizardhassan99@gmail.com>
2023-09-20 21:10:41 +0100

    commit

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -12,18 +12,18 @@ function Works() {
         <h1>coming soon<span className="dot">.</span></h1>
         <p>Try to connect with me on social media</p>
         <div className="icons">
-        <a href='https://github.com/ElazzouziHassan' target='_blank'>
-                <GitHubIcon className='s-icon github' />
-            </a>
-              <a href='https://www.linkedin.com/in/elazzouzihassan/' target='_blank'>
-                <LinkedInIcon className='s-icon linkedIn' />
-              </a>
-              <a href='https://www.facebook.com/itsmewizardy' target='_blank'>
-                <FacebookIcon className='s-icon facebook' />
-              </a>
-              <a href='https://twitter.com/itsmewizardy' target='_blank'>
-                <TwitterIcon className='s-icon twitter' />
-              </a>
+          <a href='https://github.com/ElazzouziHassan' target='_blank'>
+            <GitHubIcon className='s-icon github' />
+          </a>
+          <a href='https://www.linkedin.com/in/elazzouzihassan/' target='_blank'>
+            <LinkedInIcon className='s-icon linkedIn' />
+          </a>
+          <a href='https://www.facebook.com/itsmewizardy' target='_blank'>
+            <FacebookIcon className='s-icon facebook' />
+          </a>
+          <a href='https://twitter.com/itsmewizardy' target='_blank'>
+            <TwitterIcon className='s-icon twitter' />
+          </a>
         </div>
       </div>
     </div>
