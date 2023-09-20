ff7c90783e931a2851b9418cb532ab424cc0b0ee
wizardy <wizardhassan99@gmail.com>
2023-09-20 20:26:37 +0100

    commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function About() {
   return (
     <div className="about">
-      
+      <div class="wrapper">
+        <h1>coming soon<span class="dot">.</span></h1>
+        <p>Put some text here.</p>
+        <div class="icons">
+          <a href=""><i class="fa fa-twitter"></i></a>
+          <a href=""><i class="fa fa-youtube-play"></i></a>
+          <a href=""><i class="fa fa-paper-plane"></i></a>
+        </div>
+      </div>
     </div>
   )
 }
