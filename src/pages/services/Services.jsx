aa7afebacc52c4b0bc8ffc4aa2de99c881cda2b5
wizardy <wizardhassan99@gmail.com>
2023-09-22 09:29:00 +0100

    commit

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import './services.scss'
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Services() {
   return (
