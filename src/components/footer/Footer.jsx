b1c787d0d3f358b3a94f718daf1e913d645609cf
wizardy <wizardhassan99@gmail.com>
2023-09-29 18:36:15 +0100

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './footer.scss';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+      <h1>Footer component !</h1>
+    </div>
+    )
 }
 
 export default Footer
