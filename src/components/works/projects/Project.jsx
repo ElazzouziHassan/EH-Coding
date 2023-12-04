4c64c0d8de0fd53627deb9b2e832746ace24a148
wizardy <wizardhassan99@gmail.com>
2023-12-04 16:58:27 +0100

    commit

---
+++ src/components/works/projects/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './project.scss';
+
+function Project() {
+  return (
+    <div className="project">
+      <h1>project component !</h1>
+    </div>
+  )
+}
+
+export default Project
