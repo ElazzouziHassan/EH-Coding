9844c5ff310da70acb3b5dd599722d4063c8298a
wizardy <wizardhassan99@gmail.com>
2023-12-04 17:11:59 +0100

    commit

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -3,11 +3,39 @@ import './works.scss'
 import Sidebar from '../../components/works/side-bar/side-bar'
 import Project from '../../components/works/projects/Project'
 
+// to be removed !
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 function Works() {
   return (
     <div className="works">
-      <Sidebar/>
-      <Project/>
+      {/* <Sidebar/>
+      <Project/> */}
+
+      {/* to be removed ! */}
+
+      <div className="wrapper">
+        <h1>coming soon<span className="dot">.</span></h1>
+        <p>Try to connect with me on social media</p>
+        <div className="icons">
+          <a href='https://github.com/ElazzouziHassan' target='_blank'>
+            <GitHubIcon className='s-icon github' />
+          </a>
+          <a href='https://www.linkedin.com/in/elazzouzihassan/' target='_blank'>
+            <LinkedInIcon className='s-icon linkedIn' />
+          </a>
+          <a href='https://www.facebook.com/itsmewizardy' target='_blank'>
+            <FacebookIcon className='s-icon facebook' />
+          </a>
+          <a href='https://twitter.com/itsmewizardy' target='_blank'>
+            <TwitterIcon className='s-icon twitter' />
+          </a>
+        </div>
+      </div>
+      
     </div>
   )
 }
