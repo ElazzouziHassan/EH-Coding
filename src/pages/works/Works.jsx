5008a0d7eecc84e27d948bd3cdf382e5b841684a
wizardy <wizardhassan99@gmail.com>
2023-12-04 17:03:23 +0100

    commit

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './works.scss'
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import Sidebar from '../../components/works/side-bar/side-bar'
+import Project from '../../components/works/projects/Project'
 
 function Works() {
   return (
     <div className="works">
-      Works component !
+      <Sidebar/>
+      <Project/>
     </div>
   )
 }
