37ef3f4ce548908ca844fc297fea5c7321203b39
wizardy <wizardhassan99@gmail.com>
2024-02-22 20:39:15 +0100

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,20 @@ function Home() {
   return (
     <div className="home">
       <div className="hero-section">
-        <div className="left-side">Left Side</div>
+        <div className="left-side">
+          <div className="me">
+            <span>Hello, World ! I'm</span>
+            <h2>El azzouzi Hassan</h2>
+            <p>WEB FULL-STACK DEVELOPER</p>
+          </div>
+          <div className="short-description">
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit officia enim pariatur excepturi ipsam officiis reprehenderit veniam eum hic, quasi rerum minima atque illum accusamus. Tempora quaerat quos dolore optio?
+            </p>
+            <button>Contact me</button>
+            <button>Resume</button>
+          </div>
+        </div>
         <div className="right-side">Right Side</div>
       </div>
     </div>
