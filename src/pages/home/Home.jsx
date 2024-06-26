44e42fc94f4b900bb9fa97fcf6981e25b729fcb5
Wizardy <wizardhassan99@gmail.com>
2024-06-26 17:34:09 +0100

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,14 +6,38 @@ import Creative from '../../components/Home/creative/Creative';
 import Insights from '../../components/Home/Insights/Insights';
 import Discover from '../../components/Home/discover/Discover';
 
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 function Home() {
   return (
     <div className="home">
-      <Hero/>
-      {/* <Creative/>
+      <div className="wrapper">
+        <h1>coming soon<span className="dot">.</span></h1>
+        <p>Try to connect with me on social media</p>
+        <div className="icons">
+          <a href='https://github.com/ElazzouziHassan' target='_blank'>
+            <GitHubIcon className='s-icon github' />
+          </a>
+          <a href='https://www.linkedin.com/in/elazzouzihassan/' target='_blank'>
+            <LinkedInIcon className='s-icon linkedIn' />
+          </a>
+          <a href='https://www.facebook.com/itsmewizardy' target='_blank'>
+            <FacebookIcon className='s-icon facebook' />
+          </a>
+          <a href='https://twitter.com/itsmewizardy' target='_blank'>
+            <TwitterIcon className='s-icon twitter' />
+          </a>
+        </div>
+      </div>
+
+      {/* <Hero/>
+      <Creative/>
       <Discover/>
-      <Insights/> */}
-      <Footer/>
+      <Insights/>
+      <Footer/> */}
     </div>
   )
 }
